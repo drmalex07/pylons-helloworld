491ec472af38ec0402dc8d54cec087f5ccc55fbf
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-10-28 12:49:39 +0200

    Added examples for Nose functional tests

--- helloworld/tests/functional/test_poll.py
+++ helloworld/tests/functional/test_poll.py
@@ -3,5 +3,5 @@ from helloworld.tests import *
 class TestPollController(TestController):
 
     def test_index(self):
-        response = self.app.get(url(controller='poll', action='index'))
+        response = self.app.get(url(controller='poll', action='results'))
         # Test response...
